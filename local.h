adb82dea3215cb236aa543a2428071cb3cf3e863
Osama Rimawi <osamarimawi2002@gmail.com>
2023-05-25 12:24:49 +0300

    Update local.h

--- local.h
+++ local.h
@@ -1,48 +1,46 @@
-#ifndef __LOCAL_H_
-#define __LOCAL_H_
-
-/*
- * Common header file: parent, producer and consumer
- */
+#ifndef LOCAL_H
+#define LOCAL_H
 
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
+#include <unistd.h>
 #include <sys/ipc.h>
-#include <sys/sem.h>
 #include <sys/shm.h>
-#include <wait.h>
-#include <signal.h>
+#include <sys/sem.h>
 
-#define ROWS   5
-#define COLS   3
+#define SHM_SIZE 1024
+#define colLength 5
+#define colWidth 150
 
-#define SLOT_LEN  50
-#define N_SLOTS   6
+typedef struct
+{
+  char message[colLength][colWidth];
+} SharedMemory;
 
-/* This declaration is *MISSING* is many solaris environments.
-   It should be in the <sys/sem.h> file but often is not! If 
-   you receive a duplicate definition error message for semun
-   then comment out the union declaration.
-   */
-
-union semun {
-  int              val;
+union semun
+{
+  int val;
   struct semid_ds *buf;
-  ushort          *array; 
+  unsigned short *array;
 };
 
-struct MEMORY {
-  char buffer[N_SLOTS][SLOT_LEN];
-  int  head, tail;
-};
+void wait_semaphore(int sem_id);
+void signal_semaphore(int sem_id);
+void wait_semaphore(int sem_id)
+{
+  struct sembuf operation;
+  operation.sem_num = 0;
+  operation.sem_op = -1;
+  operation.sem_flg = SEM_UNDO;
+  semop(sem_id, &operation, 1);
+}
 
-struct sembuf acquire = {0, -1, SEM_UNDO}, 
-              release = {0,  1, SEM_UNDO};
-
-enum {AVAIL_SLOTS, TO_CONSUME};
-
-#endif
-
+void signal_semaphore(int sem_id)
+{
+  struct sembuf operation;
+  operation.sem_num = 0;
+  operation.sem_op = 1;
+  operation.sem_flg = SEM_UNDO;
+  semop(sem_id, &operation, 1);
+}
+#endif /* LOCAL_H */
